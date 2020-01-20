8efc0e479859e3588ff7bda85f6f63ca9b6c40a5
Chiaen Fu <peterfuwork@gmail.com>
2020-01-19 21:41:21 -0700

    added 248 dishes information to api, updated the announcement component infomation

--- src/Announcement.js
+++ src/Announcement.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Announcement = (props) => {
 	return (
 		<div className={props.note}>
-			<div className="note">公告：16道春節料理大賽新食譜已更新</div>
+			<div className="note">
+				公告：總料理已更新至248道,其中16道為春節料理大賽新食譜
+			</div>
 			<span className="button" onClick={() => props.deleteAnnouncement()}>
 				&#10005;
 			</span>
