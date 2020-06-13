1ac01feeaf5e633b74e249dc786777187baec68c
michelle61219 <michelle61219@gmail.com>
2020-06-13 14:28:53 -0600

    updated dish photos

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 module.exports = {
-	api: 'https://api.jsonbin.io/b/5ede999f1f9e4e578819e010/1',
+	api: 'https://api.jsonbin.io/b/5ede999f1f9e4e578819e010/3',
 	secretKey: '$2b$10$/w1XilSCLMjq5y3vYWVs7.qLOQdCcILMlpoluDM9/jVrnpnWdrDnu',
 };
