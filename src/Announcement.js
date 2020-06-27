1c3cfce9da89a0fd67eadc3c8dfc8bfde247ef2d
michelle61219 <michelle61219@gmail.com>
2020-06-27 14:50:14 -0600

    added new dishes and seasonal dishes

--- src/Announcement.js
+++ src/Announcement.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Announcement = (props) => {
 	return (
 		<div className={props.note}>
-			<div className="note">公告：總料理更新至248道,8道為元宵節料理食譜</div>
+			<div className="note">
+				<div>
+					公告：已更新6月後新食譜: 5道新料理、6道魚、18道檸檬、23道馬鈴薯料理!
+				</div>
+				<div>NEW !!! 9道2020年端午節料理 ~</div>
+			</div>
 			<span className="button" onClick={() => props.deleteAnnouncement()}>
 				&#10005;
 			</span>
--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 module.exports = {
-	api: 'https://api.jsonbin.io/b/5ede999f1f9e4e578819e010/3',
+	api: 'https://api.jsonbin.io/b/5ede999f1f9e4e578819e010/11',
 	secretKey: '$2b$10$/w1XilSCLMjq5y3vYWVs7.qLOQdCcILMlpoluDM9/jVrnpnWdrDnu',
 };
